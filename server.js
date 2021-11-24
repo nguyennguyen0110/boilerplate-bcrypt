b701f18a92f72039cec86d6c145cb14c1eb34a58
nguyennguyen0110 <85159841+nguyennguyen0110@users.noreply.github.com>
2021-11-24 16:10:32 +0700

    Update server.js

--- server.js
+++ server.js
@@ -7,47 +7,21 @@ fccTesting(app);
 const saltRounds = 12;
 const myPlaintextPassword = 'sUperpassw0rd!';
 const someOtherPlaintextPassword = 'pass123';
-
+const bcrypt = require('bcrypt');
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
-
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+  console.log(hash);
+  bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+    console.log(res); //true
+  });
+});
 //END_ASYNC
 
 //START_SYNC
-
-
-
+let hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+let result = bcrypt.compareSync(myPlaintextPassword, hash);
 //END_SYNC
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(process.env.PORT || 3000, () => {});
